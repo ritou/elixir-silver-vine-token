014d53b6f0c5ee54a04abd458c5b79dbb0a13fdb
ritou <ritou.06@gmail.com>
2021-10-18 15:49:13 +0900

    add description to token module

--- lib/silver_vine/token.ex
+++ lib/silver_vine/token.ex
@@ -1,6 +1,18 @@
 defmodule SilverVine.Token do
   @moduledoc """
-  Documentation for `SilverVine.Token`.
+  SilverVine.Token provides functions for implementing various OAuth 2.0 / OpenID Connect tokens using JWT.
+
+  * OAuth 2.0
+  * Access Token : Bearer, DPoP
+  * Refresh Token
+  * Authorization Code
+  * OIDC
+  * ID Token (TODO)
+
+  The format of the access token is based on ["JSON Web Token (JWT) Profile for OAuth 2.0 Access Tokens"](https://datatracker.ietf.org/doc/html/draft-ietf-oauth-access-token-jwt), and the Authorization Code/Refresh Token is expressed in JWT in a similar format.
+
+  Using JWT for OAuth tokens does not mean that simple statelessness applies to the use case.
+  The ability to handle metadata such as token usage, expiration date, issuer, etc. in the JWT layer will simplify the implementation required for OAuth 2.0 Authorization Server/Resource Server applications.
   """
 
   alias KittenBlue.{JWS, JWK}
