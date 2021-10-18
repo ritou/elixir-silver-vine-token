21b496ef45d3cff7841a0937ec7ccd46af04bb55
ritou <ritou.06@gmail.com>
2021-10-18 16:10:14 +0900

    update for release 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 0.1.0 (2021-10-18)
+
+* Initial Release
+* Define tokens for OAuth 2.0
--- mix.exs
+++ mix.exs
@@ -5,8 +5,15 @@ defmodule SilverVine.Token.MixProject do
     [
       app: :silver_vine_token,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
+      description:
+        "SilverVine.Token provides functions for implementing various OAuth 2.0 / OpenID Connect tokens using JWT.",
+      package: [
+        maintainers: ["Ryo Ito"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/ritou/elixir-silver-vine-token"}
+      ],
       deps: deps()
     ]
   end
@@ -24,8 +31,9 @@ defmodule SilverVine.Token.MixProject do
       {:jason, "~> 1.2"},
       {:kitten_blue, "~> 0.5"},
       {:uuid, "~> 1.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+
+      # for docs
+      {:ex_doc, "~> 0.25.3", only: :dev, runtime: false}
     ]
   end
 end
