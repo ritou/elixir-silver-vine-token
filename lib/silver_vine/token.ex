b55dde5691a9f96d32570fa329a664dd1652f1de
ritou <ritou.06@gmail.com>
2021-10-18 16:20:09 +0900

    fix doc for token

--- lib/silver_vine/token.ex
+++ lib/silver_vine/token.ex
@@ -24,7 +24,8 @@ defmodule SilverVine.Token do
   @spec put_jti(payload :: map) :: map()
   def put_jti(payload), do: Map.put(payload, "jti", UUID.uuid4())
 
-  @spec verify(token :: String.t(), keys :: List.t(), typ :: String.t()) :: {:ok, payload :: map}
+  @spec verify(token :: String.t(), keys :: list(JWK.t()), typ :: String.t()) ::
+          {:ok, payload :: map}
   def verify(token, keys, typ), do: JWS.verify(token, keys, typ_to_map(typ))
 
   defp typ_to_map(typ), do: %{"typ" => typ}
